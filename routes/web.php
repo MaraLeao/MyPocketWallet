d6ebe1c106d1e1a450003b927d47724398745321
Clara <mariaclagouv@gmail.com>
2025-07-16 16:50:50 -0300

    Feat: Add routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EntradaController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/entradas', [EntradaController::class, 'index']);
+
+Route::get('/users/{id}', [UserController::class, 'show']);
