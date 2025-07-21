a6fccaaf6d69652569f5d5ef6366ad3690f15602
Clara <mariaclagouv@gmail.com>
2025-07-21 19:07:31 -0300

    Feat: Add FP and Entrada routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FPController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EntradaController;
 use App\Http\Controllers\UserController;
@@ -23,5 +24,11 @@ Route::get('/', function () {
 Route::get('/entradas', [EntradaController::class, 'index']);
 Route::get('/entradas/create/despesa', [EntradaController::class, 'createDespesa']);
 Route::get('/entradas/create/ganho', [EntradaController::class, 'createGanho']);
+Route::get('/entradas/edit/{id}', [EntradaController::class, 'edit']);
+
+Route::get('/formapagamentos', [FPController::class, 'index']);
+Route::get('/formapagamentos/create', [FPController::class, 'create']);
+Route::get('/formapagamentos/edit/{id}', [FPController::class, 'edit']);
+Route::get('/formapagamentos/delete/{id}', [FPController::class, 'delete']);
 
 Route::get('/users/{id}', [UserController::class, 'show']);
